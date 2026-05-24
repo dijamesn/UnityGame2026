85703565b1f08a42816a2196567a6bd3d1ba6b2f
dijamesn <78842909+dijamesn@users.noreply.github.com>
2026-05-25 09:18:03 +1000

    Disable MouseLook when FPS camera is inactive (camera toggle support)

--- MouseLook.cs
+++ MouseLook.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+// Attach this to the FPS Camera (child of the Player).
+// It automatically disables itself when the FPS camera's GameObject is inactive,
+// so it won't conflict with the overhead camera mode.
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
@@ -9,6 +12,10 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
+        // Do nothing when this camera's GameObject has been deactivated by CameraController
+        if (!gameObject.activeInHierarchy)
+            return;
+
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
